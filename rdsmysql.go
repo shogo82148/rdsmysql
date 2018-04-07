0e7c618d0d071371ea53a9ab14da24434d1c0794
Ichinose Shogo <shogo82148@gmail.com>
2018-04-07 15:09:12 +0900

    simplify the driver setting.
    If you want to assume role, use stscreds.
    https://docs.aws.amazon.com/sdk-for-go/api/aws/credentials/stscreds/

--- rdsmysql.go
+++ rdsmysql.go
@@ -2,13 +2,10 @@ package rdsmysql
 
 import (
 	"database/sql/driver"
-	"fmt"
+	"errors"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/rds/rdsutils"
-	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/go-sql-driver/mysql"
 	pkgerrors "github.com/pkg/errors"
 )
@@ -16,10 +13,7 @@ import (
 // Driver is mysql driver using IAM DB Auth.
 // https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/UsingWithRDS.IAMDBAuth.html
 type Driver struct {
-	AWSSession *session.Session
-
-	// If role is set, do sts:AssumeRole before getting the token.
-	Role string
+	Session *session.Session
 }
 
 // Open opens new connection.
@@ -29,36 +23,12 @@ func (d *Driver) Open(name string) (driver.Conn, error) {
 		return nil, pkgerrors.Wrap(err, "fail to parse dns")
 	}
 
-	// Get Credentials
-	var cred *credentials.Credentials
-	if d.Role == "" {
-		cred = d.AWSSession.Config.Credentials
-	} else {
-		stssvc := sts.New(d.AWSSession)
-		caller, err := stssvc.GetCallerIdentity(&sts.GetCallerIdentityInput{})
-		if err != nil {
-			return nil, pkgerrors.Wrap(err, "fail to get call identity")
-		}
-		res, err := stssvc.AssumeRole(&sts.AssumeRoleInput{
-			RoleArn: aws.String(
-				fmt.Sprintf(
-					"arn:aws:iam::%s:role/"+d.Role,
-					*caller.Account,
-				),
-			),
-			RoleSessionName: aws.String("connect"),
-		})
-		if err != nil {
-			return nil, pkgerrors.Wrap(err, "fail to assume role")
-		}
-		cred = credentials.NewStaticCredentials(
-			*res.Credentials.AccessKeyId,
-			*res.Credentials.SecretAccessKey,
-			*res.Credentials.SessionToken,
-		)
+	cred := d.Session.Config.Credentials
+	if d.Session.Config.Region == nil {
+		return nil, errors.New("rdsmysql: region is missing")
 	}
 
-	token, err := rdsutils.BuildAuthToken(config.Addr, *d.AWSSession.Config.Region, config.User, cred)
+	token, err := rdsutils.BuildAuthToken(config.Addr, *d.Session.Config.Region, config.User, cred)
 	if err != nil {
 		return nil, pkgerrors.Wrap(err, "fail to build auth token")
 	}
