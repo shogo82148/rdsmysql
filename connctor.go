d1a77383597ca4ccdbf1ec0ca164d9ffd0370919
Ichinose Shogo <shogo82148@gmail.com>
2019-03-26 17:55:03 +0900

    rate limit new connections

--- connctor.go
+++ connctor.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"database/sql/driver"
 	"errors"
+	"sync"
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/rds/rdsutils"
 	"github.com/go-sql-driver/mysql"
 	pkgerrors "github.com/pkg/errors"
 	_ "github.com/shogo82148/rdsmysql/internal/certificate" // install certificate.
+	"golang.org/x/time/rate"
 )
 
 // check Connector implements driver.Connctor.
@@ -17,12 +19,16 @@ var _ driver.Connector = &Connector{}
 
 // Connector is an implementation of driver.Connector
 type Connector struct {
-	Session *session.Session
-	Config  *mysql.Config
+	Session           *session.Session
+	Config            *mysql.Config
+	MaxConnsPerSecond int
+
+	mu      sync.Mutex
+	limiter *rate.Limiter
 }
 
 // Connect returns a connection to the database.
-func (c *Connector) Connect(context.Context) (driver.Conn, error) {
+func (c *Connector) Connect(ctx context.Context) (driver.Conn, error) {
 	config := *c.Config // shollow copy
 
 	cred := c.Session.Config.Credentials
@@ -40,7 +46,27 @@ func (c *Connector) Connect(context.Context) (driver.Conn, error) {
 	config.Passwd = token
 	config.TLSConfig = "rdsmysql"
 
+	if l := c.getlimiter(); l != nil {
+		if err := l.Wait(ctx); err != nil {
+			return nil, err
+		}
+	}
+
 	return (&mysql.MySQLDriver{}).Open(config.FormatDSN())
+}
+
+func (c *Connector) getlimiter() *rate.Limiter {
+	if c.MaxConnsPerSecond == 0 {
+		return nil
+	}
+	c.mu.Lock()
+	limiter := c.limiter
+	if limiter == nil {
+		limiter = rate.NewLimiter(rate.Limit(c.MaxConnsPerSecond), 1)
+		c.limiter = limiter
+	}
+	c.mu.Unlock()
+	return limiter
 }
 
 // Driver returns the underlying Driver of the Connector.
--- go.mod
+++ go.mod
@@ -4,4 +4,5 @@ require (
 	github.com/aws/aws-sdk-go v1.16.18
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/pkg/errors v0.8.1
+	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4
 )
