99ac12c8cc42f70822cc98babc7d3dba44511e99
ICHINOSE Shogo <shogo82148@gmail.com>
2023-05-07 22:20:46 +0900

    add RDS instance

--- _integration/lib/_integration-stack.ts
+++ _integration/lib/_integration-stack.ts
@@ -1,16 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
 
 export class IntegrationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          name: 'public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'IntegrationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const rdsInstance = new rds.DatabaseInstance(this, 'MyDatabaseInstance', {
+      engine: rds.DatabaseInstanceEngine.mysql({
+        version: rds.MysqlEngineVersion.VER_8_0_32
+      }),
+      allocatedStorage: 100,
+      credentials: {
+        username: 'admin',
+        password: cdk.SecretValue.unsafePlainText('secret'),
+      },
+      deletionProtection: true,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      vpc: vpc
+    });
   }
 }
