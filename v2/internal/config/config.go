1b3b7e45e3d308c4d139a850f6c9abd01a888730
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-10 00:18:05 +0900

    remove ioutil package
    the io/ioutil package is now deprecated

--- v2/internal/config/config.go
+++ v2/internal/config/config.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -38,10 +37,10 @@ enable-cleartext-plugin
 `, config.Host, config.User, config.Port, token, pempath)
 
 	now := time.Now()
-	if err := ioutil.WriteFile(fmt.Sprintf("%s.%d", confpath, now.UnixNano()), []byte(conf), 0600); err != nil {
+	if err := os.WriteFile(fmt.Sprintf("%s.%d", confpath, now.UnixNano()), []byte(conf), 0600); err != nil {
 		return err
 	}
-	if err := ioutil.WriteFile(fmt.Sprintf("%s.%d", pempath, now.UnixNano()), []byte(certificate.Certificate), 0600); err != nil {
+	if err := os.WriteFile(fmt.Sprintf("%s.%d", pempath, now.UnixNano()), []byte(certificate.Certificate), 0600); err != nil {
 		return err
 	}
 
