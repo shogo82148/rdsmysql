1c3b19d8b626dfc13fe7c8ba0dd7c078ead59e29
ICHINOSE Shogo <shogo82148@gmail.com>
2024-03-10 22:15:11 +0900

    Refactor certificate parsing in test file

--- certificate_test.go
+++ certificate_test.go
@@ -2,14 +2,27 @@ package rdsmysql
 
 import (
 	"crypto/x509"
+	"encoding/pem"
 	"testing"
 )
 
 func TestCertificate(t *testing.T) {
 	t.Parallel()
 
-	certPool := x509.NewCertPool()
-	if ok := certPool.AppendCertsFromPEM([]byte(Certificate)); !ok {
-		t.Error("failed to append certs")
+	pemCerts := []byte(Certificate)
+	for len(pemCerts) > 0 {
+		var block *pem.Block
+		block, pemCerts = pem.Decode(pemCerts)
+		if block == nil {
+			break
+		}
+
+		if block.Type != "CERTIFICATE" || len(block.Headers) != 0 {
+			t.Errorf("unexpected block type: %s", block.Type)
+			continue
+		}
+		if _, err := x509.ParseCertificate(block.Bytes); err != nil {
+			t.Errorf("failed to parse certificate: %v", err)
+		}
 	}
 }
